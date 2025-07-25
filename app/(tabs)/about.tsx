7fd3d5b61d0e5bd7dddfe3b09dce35b7d2f41bca
Alan Mendelevich <ailon@ailon.org>
2025-07-25 16:23:19 +0300

    add basic demo page content

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,9 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Linking, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>About screen</Text>
+      <Text style={styles.text}>marker.js for React Native Demo</Text>
+      <Pressable
+        onPress={() =>
+          Linking.openURL(
+            "https://markerjs.com/react-native?utm_source=markerjsrn-demo&utm_medium=about"
+          )
+        }
+        style={{ padding: 8 }}
+      >
+        <Text style={styles.linkText}>Learn more</Text>
+      </Pressable>
     </View>
   );
 }
@@ -17,5 +27,12 @@ const styles = StyleSheet.create({
   },
   text: {
     color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  linkText: {
+    color: "#E60076",
+    fontSize: 16,
+    textDecorationLine: "underline",
   },
 });
